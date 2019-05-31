25e08b89d0d562c1608cb0650f47f9d311eedf9c
Ashik Shafayet <ashik19aug@gmail.com>
2019-06-01 02:56:12 +0600

    Add files via upload

---
+++ JAVA/Basic/A_hello.java
@@ -0,0 +1,7 @@
+//Ashik Shafayet
+
+public class A_hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ JAVA/Basic/C_TypeCasting.java
@@ -0,0 +1,18 @@
+
+//Ashik Shafayet
+
+public class C_TypeCasting {
+	public static void main(String[] args)
+	{
+		int a=15;
+		double d = a; //Autometic Type Casting
+		
+		
+		double d1 = 12.5;
+		int a1 = (int) d1;
+		
+		System.out.println(d);
+		System.out.println(a1);
+	}
+
+}
